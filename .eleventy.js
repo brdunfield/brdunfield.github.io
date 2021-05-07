c104628d4f33423fb55ef2e16e855e5200eead73
brdunfield <bethany.dunfield@cds-snc.ca>
2021-05-07 13:37:14 -0600

    Basic styles

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setTemplateFormats([
+    "jpeg",
+    "jpg",
+    "png",
+    "html",
+    "md",
+    "njk",
+    "11ty.js"
+  ]);
+};
